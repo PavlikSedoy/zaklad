41937cba7e4d9567d978bb7d36bd6dc59a87a7d8
PavelSedoy <pavliksedoy@gmail.com>
2019-11-10 20:43:42 +0200

    Home page. Deals section

---
+++ dev/static/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.home-slider-container', {
+        // Optional parameters
+        loop: true,
+        autoplay: {
+            delay: 3000,
+        },
+        effect: "fade"
+    })
+});
