c157463600dfdec6e84a39d1bf07131eec4ed874
PavelSedoy <pavliksedoy@gmail.com>
2019-11-11 18:18:40 +0200

    Publications gallery

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -10,7 +10,30 @@ $(document).ready(function () {
     })
 
     // Gallery init
-    lightGallery(document.getElementById('gallery'), {
+    lightGallery(document.getElementById('galler'), {
         thumbnail:true
-    }); 
+    });
+
+    // Publications gallery
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 15,
+        slidesPerView: 6,
+        // loop: true,
+        // freeMode: true,
+        // loopedSlides: 5, //looped slides should be the same
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 0,
+        // loop:true,
+        // loopedSlides: 5, //looped slides should be the same
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs,
+        },
+    });  
 });
