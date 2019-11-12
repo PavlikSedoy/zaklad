6c9f43d242e3e7699bfa950aa2a7c9985173b069
PavelSedoy <pavliksedoy@gmail.com>
2019-11-12 14:29:08 +0200

    Front-end

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     })
 
     // Gallery init
-    lightGallery(document.getElementById('galler'), {
+    lightGallery(document.getElementById('gallery'), {
         thumbnail:true
     });
 
@@ -23,6 +23,16 @@ $(document).ready(function () {
         // loopedSlides: 5, //looped slides should be the same
         watchSlidesVisibility: true,
         watchSlidesProgress: true,
+        breakpoints: {
+            320: {
+              slidesPerView: 3,
+              spaceBetween: 5,
+            },
+            768: {
+                slidesPerView: 6,
+                spaceBetween: 15,
+            }
+        }
     });
     var galleryTop = new Swiper('.gallery-top', {
         spaceBetween: 0,
@@ -35,5 +45,50 @@ $(document).ready(function () {
         thumbs: {
             swiper: galleryThumbs,
         },
-    });  
+    });
+
+    // FAQ
+    $('.faq__item-header').click( function() {
+        $(this).next('.faq__item-content').slideToggle();
+        $(this).parent().toggleClass('active');
+    });
+
+    // Open popup
+    $('.btn').click( function(e) {
+        e.stopPropagation();
+        $('.popup').fadeOut();
+        $('.mobile-nav').fadeOut();
+        if ($(this).attr('data-type') == 'modal') {
+            if ($(this).attr('data-modal') == 'request') $('#requestPopup').fadeIn();
+            if ($(this).attr('data-modal') == 'callback') $('#callbackPopup').fadeIn();
+            if ($(this).attr('data-modal') == 'review') $('#reviewPopup').fadeIn();
+        }
+    });
+
+    // Close popup
+    $('.popup__close').click( function(e) {
+        $('.popup').fadeOut();
+    });
+
+    $(document).click( function(e) {
+        if (!$(e.target).parents().addBack().is('.popup__modal')) {
+            $('.popup').fadeOut();
+        }
+    });
+    
+    $(document).keyup(function(e) {
+        if (e.key === "Escape") {
+            $('.popup').fadeOut();
+        }
+    });
+
+    // Open mobile nav
+    $('#hamburger').click( function() {
+        $('.mobile-nav').fadeIn();
+    });
+
+    // Close mobile nav
+    $('.mobile-nav__close').click( function() {
+        $('.mobile-nav').fadeOut();
+    });
 });
